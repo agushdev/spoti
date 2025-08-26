a42d5b4bedc870a6be1a6b63c8ffab206b34ae0f
agushdev <agusponce055@gmail.com>
2025-08-26 18:44:36 -0300

    Update PWA offline

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -5,6 +5,34 @@ const withPWA = withPWAInit({
   register: true, 
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/, // Expresión regular para que coincida con todas las peticiones
+      handler: 'NetworkFirst', // Estrategia por defecto
+      options: {
+        cacheName: 'http-cache',
+        expiration: {
+          maxEntries: 10,
+          maxAgeSeconds: 60 * 60 * 24 * 7, // 7 días
+        },
+        cacheableResponse: {
+          statuses: [0, 200],
+        },
+      },
+    },
+    {
+      // Esta es la parte importante para cachear las canciones
+      // Usa una URL que coincida con tus archivos de audio, por ejemplo de Dropbox
+      urlPattern: /\.(jpg|mp3|wav|ogg)$/i,
+      handler: 'CacheFirst', // Primero la caché, si no está, la red
+      options: {
+        cacheName: 'audio-cache', // Un nombre específico para la caché de audios
+        expiration: {
+          maxEntries: 50, // Límite de audios en caché
+        },
+      },
+    },
+  ],
 });
 
 /** @type {import('next').NextConfig} */
